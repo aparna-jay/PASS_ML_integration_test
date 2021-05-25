39830845c020643ee334574bcf326d6401121f42
aparna-jay <aparna.jayawardena1999@gmail.com>
2021-05-25 23:26:13 +0530

    Initial commit

---
+++ pass ml integration data/convert.py
@@ -0,0 +1,12 @@
+from tensorflow.keras.models import load_model
+import tensorflow as tf
+
+model = load_model("catdog.h5")
+
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+
+print("model converted")
+
+with open('model.tflite', 'wb') as f:
+	f.write(tflite_model)
