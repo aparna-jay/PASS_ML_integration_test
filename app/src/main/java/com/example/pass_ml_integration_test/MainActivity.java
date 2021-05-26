bf98f6b29a9d0598cc434a8a676fe1ad63287625
aparna-jay <aparna.jayawardena1999@gmail.com>
2021-05-26 14:29:52 +0530

    tflite model changed and tested

--- app/src/main/java/com/example/pass_ml_integration_test/MainActivity.java
+++ app/src/main/java/com/example/pass_ml_integration_test/MainActivity.java
@@ -1,87 +1,74 @@
 package com.example.pass_ml_integration_test;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import com.example.pass_ml_integration_test.ml.Model;
-
 import org.tensorflow.lite.DataType;
-import org.tensorflow.lite.support.image.TensorImage;
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
-
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.time.temporal.Temporal;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView imgView;
-    private Button select, predict;
-    private TextView tv;
-    private Bitmap bitmap;
-    private String animal;
+    private Button predict;
+    private TextView one, two , result;
+    private float[] numbers = {1, 2, 3, 2, 3, 1, 1, 2, 3, 2, 3, 1, 1, 2, 3, 2, 3, 1, 2, 1, 2};
+    private String output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imgView =  (ImageView) findViewById(R.id.imageView);
-        select = (Button) findViewById(R.id.button);
         predict = (Button) findViewById(R.id.button2);
-        tv = (TextView) findViewById(R.id.textView);
-
-        select.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                    intent.setType("image/*");
-                    startActivityForResult(intent, 100);
-            }
-        });
+        one = (TextView) findViewById(R.id.one);
+        two = (TextView) findViewById(R.id.two);
+        result = (TextView) findViewById(R.id.result);
 
         predict.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                bitmap = Bitmap.createScaledBitmap(bitmap, 128, 128, true);
 
                 try {
                     Model model = Model.newInstance(getApplicationContext());
 
                     // Creates inputs for reference.
-                    TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 128, 128, 3}, DataType.FLOAT32);
+                    TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 21}, DataType.FLOAT32);
 
-                    TensorImage tensorImage = new TensorImage(DataType.FLOAT32);
-                    tensorImage.load(bitmap);
-                    ByteBuffer byteBuffer = tensorImage.getBuffer();
+                  //  ByteBuffer.allocate(4).putFloat(numbers[0]).array();
+                    byte[] byteArray= FloatArray2ByteArray(numbers);
+                    ByteBuffer byteBuffer= ByteBuffer.wrap(byteArray);
 
                     inputFeature0.loadBuffer(byteBuffer);
 
                     // Runs model inference and gets result.
                     Model.Outputs outputs = model.process(inputFeature0);
                     TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+                    String converted= new String(byteBuffer.array(), "UTF-8");
+
+
+                    if (outputFeature0.getFloatArray()[0] > outputFeature0.getFloatArray()[1]){
+                        output = "Autistic";
+                    }
+                    else if(outputFeature0.getFloatArray()[1] > outputFeature0.getFloatArray()[0]){
+                        output = "Not autistic";
+                  }
+
+                    one.setText("" + outputFeature0.getFloatArray()[0]);
+                    two.setText("" + outputFeature0.getFloatArray()[1]) ;
+                    result.setText(output);
+                    Toast.makeText(MainActivity.this, "output:" + outputFeature0.getFloatArray()[0] + " , " + outputFeature0.getFloatArray()[1], Toast.LENGTH_SHORT).show();
 
                     // Releases model resources if no longer used.
                     model.close();
 
-                    if (outputFeature0.getFloatArray()[0] >= 1.0){
-                        animal = "Cat";
-                    }
-                    else if(outputFeature0.getFloatArray()[1] >= 1.0){
-                        animal = "Dog";
-                    }
 
-                    tv.setText(animal);
+
 
                 } catch (IOException e) {
                     // TODO Handle the exception
@@ -89,21 +76,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == 100){
-            imgView.setImageURI(data.getData());
-
-            Uri uri = data.getData();
-            try {
-                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
+    public static byte[] FloatArray2ByteArray(float[] values){
+        ByteBuffer buffer= ByteBuffer.allocate(4 * values.length);
+        for (float value : values){
+            buffer.putFloat(value);
         }
+        return buffer.array();
     }
+
 }
